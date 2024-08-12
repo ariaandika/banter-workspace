805a6b4d74ce5c0bd63cc8e225f5086b213be7b2
ariaandika <andikaakun17@gmail.com>
2024-08-12 19:59:22 +0700

    test run with sqlx

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,13 +1,19 @@
+use std::sync::Arc;
+use sqlx::{postgres::PgRow, prelude::*};
 use http_core::{*, util::*};
+use sqlx::PgPool;
 
 
 
-pub fn router(parts: &Parts, _: Body) -> Result {
+pub async fn router(parts: &Parts, _: Body, state: Arc<PgPool>) -> Result {
     let path = normalize_path(parts.uri.path());
 
     match (&parts.method, path) {
         (GET, "/") => {
-            json!({ "name": "John" }).into_response()
+            let us = sqlx::query(sql::SELECT_USERS)
+                .map(|e: PgRow|e.get::<String, _>("name"))
+                .fetch_all(&*state).await.fatal()?;
+            us.into_response()
         }
         _ => NOT_FOUND,
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{env::var, future::Future, pin::Pin, process};
+use std::{env::var, future::Future, pin::Pin, process, sync::Arc};
 use hyper::{server::conn::http1::Builder, service::Service};
 use hyper_util::rt::TokioIo;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use tokio::{net::TcpListener, runtime::Builder as Tokio, spawn};
 use http_core::{Request, Response};
 
@@ -11,6 +12,8 @@ fn main()  {
 }
 
 async fn server() {
+    let _ = dotenvy::dotenv();
+
     let tcp = {
         let addr = format!("{}:{}",
             var("HOST").as_deref().unwrap_or("127.0.0.1"),
@@ -26,24 +29,35 @@ async fn server() {
             }
         }
     };
+
+    let pg_pool = match var("DATABASE_URL") {
+        Ok(db_url) => PgPoolOptions::new().connect_lazy(&db_url).expect("infallible"),
+        Err(_) => {
+            eprintln!("DATABASE_URL env is required");
+            process::exit(1);
+        },
+    };
+
+    let state = Arc::new(pg_pool);
+
     loop {
         let Ok((io, _)) = tcp.accept().await else { continue };
-        spawn(Builder::new().serve_connection(TokioIo::new(io), Server));
+        spawn(Builder::new().serve_connection(TokioIo::new(io), Server(Arc::clone(&state))));
     }
 }
 
-pub struct Server;
+pub struct Server(Arc<PgPool>);
 
 impl Service<Request> for Server {
     type Response = Response;
     type Error = hyper::Error;
     type Future = Pin<Box<dyn Future<Output = hyper::Result<Response>> + Send>>;
-    fn call(&self, req: Request) -> Self::Future { Box::pin(router(req)) }
+    fn call(&self, req: Request) -> Self::Future { Box::pin(router(req, Arc::clone(&self.0))) }
 }
 
-async fn router(req: Request) -> hyper::Result<Response> {
+async fn router(req: Request, state: Arc<PgPool>) -> hyper::Result<Response> {
     let (parts, body) = req.into_parts();
-    match api::router(&parts, body) {
+    match api::router(&parts, body, state).await {
         Ok(ok) => Ok(ok),
         Err(err) => Ok(err.into_response())
     }
