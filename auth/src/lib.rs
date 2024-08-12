6cf3476b1952d97e7a70e01d85ad7617949f3755
ariaandika <andikaakun17@gmail.com>
2024-08-12 21:47:39 +0700

    add from token str for auth

--- auth/src/lib.rs
+++ auth/src/lib.rs
@@ -1,4 +1,8 @@
-use types::{Serialize, Deserialize, UserId, Date, Role};
+use std::fmt::{Debug, Display};
+
+use serde::de::DeserializeOwned;
+use serde_json::Value;
+use types::{Date, Deserialize, Role, Serialize, UserId, Users};
 use argon2::{password_hash::{Error::Password, Result as ArgonResult}, Argon2, PasswordHash, PasswordVerifier as _};
 
 pub const DUMMY_PASSWD: &str = "$argon2id$v=19$m=19456,t=2,p=1$jZlzXaKWE9bOcXz99qDobg$L8MH9ZkgV/gdIhWQ72tNhDhmX4gPkdlzIUNfIF2oO4k";
@@ -9,9 +13,45 @@ pub struct Token {
     pub name: String,
     pub phone: String,
     pub role: Role,
+    pub role_data: Value,
+    pub metadata: Value,
     pub created_at: Date,
     pub updated_at: Date,
     pub verified_at: Option<Date>,
+}
+
+impl Token {
+    pub fn new(user: Users, role_data: Value) -> Self {
+        Self {
+            user_id: user.user_id,
+            name: user.name,
+            phone: user.phone,
+            role: user.role,
+            role_data,
+            metadata: user.metadata,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+            verified_at: user.verified_at,
+        }
+    }
+
+    pub fn from_token_str(key: &str, token_str: &str) -> Result<Self> {
+        let Some(body) = sign::verify(key, token_str) else {
+            return Err(Error::Unauthorized);
+        };
+
+        match serde_json::from_str::<Token>(&body) {
+            Ok(token) => Ok(token),
+            _ => Err(Error::InvalidToken),
+        }
+    }
+
+    pub fn split<T>(mut self) -> Result<(Self, T)> where T: DeserializeOwned {
+        match serde_json::from_value(self.role_data.take()) {
+            Ok(role_data) => Ok((self,role_data)),
+            _ => Err(Error::InvalidToken)
+        }
+    }
 }
 
 /// This is cpu bound process, call it with [`tokio::task::spawn_blocking`]
@@ -45,6 +85,38 @@ pub fn mock_verify(password: &str) -> ArgonResult<()> {
     match passwd_ok {
         Err(Password) => Ok(()),
         e => e
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub enum Error {
+    Unauthorized,
+    Forbidden,
+    InvalidToken
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let m = match self {
+            Error::Unauthorized => "Unauthorized",
+            Error::Forbidden => "Forbidden",
+            Error::InvalidToken => "InvalidToken",
+        };
+
+        write!(f, "{m}")
+    }
+}
+
+impl Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let m = match self {
+            Error::Unauthorized => "Authentication Required",
+            Error::Forbidden => "You are not allowed to access this resource",
+            Error::InvalidToken => "Token invalid, please issue a new token",
+        };
+
+        write!(f, "{m}")
     }
 }
 
