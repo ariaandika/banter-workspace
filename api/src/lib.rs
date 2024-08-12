7e0ee36a61ad78d7df0b2e6737b49642ae634358
ariaandika <andikaakun17@gmail.com>
2024-08-12 22:10:27 +0700

    test run for authentication

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -3,7 +3,9 @@ use sqlx::{postgres::PgRow, prelude::*};
 use http_core::{*, util::*};
 use sqlx::PgPool;
 
+const SESSION_KEY: &str = "access_token";
 
+const JWT_TOKEN: &str = "jwt";
 
 pub async fn router(parts: &Parts, _: Body, state: Arc<PgPool>) -> Result {
     let path = normalize_path(parts.uri.path());
@@ -15,11 +17,8 @@ pub async fn router(parts: &Parts, _: Body, state: Arc<PgPool>) -> Result {
                 .fetch_all(&*state).await.fatal()?;
             us.into_response()
         }
+        (GET, "/auth") => session(JWT_TOKEN, SESSION_KEY, &parts)?.into_response(),
         _ => NOT_FOUND,
     }
 }
-
-
-
-
 
