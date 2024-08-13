a0951756887f4160f061ecedb70a552cdf9d19ff
ariaandika <andikaakun17@gmail.com>
2024-08-13 09:37:33 +0700

    test run for post request

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,13 +1,16 @@
 use std::sync::Arc;
+use http_body_util::BodyExt;
+use serde::Serialize;
 use sqlx::{postgres::PgRow, prelude::*};
 use http_core::{*, util::*};
 use sqlx::PgPool;
+use types::Deserialize;
 
 const SESSION_KEY: &str = "access_token";
 
 const JWT_TOKEN: &str = "jwt";
 
-pub async fn router(parts: &Parts, _: Body, state: Arc<PgPool>) -> Result {
+pub async fn router(parts: &Parts, body: Body, state: Arc<PgPool>) -> Result {
     let path = normalize_path(parts.uri.path());
 
     match (&parts.method, path) {
@@ -18,6 +21,20 @@ pub async fn router(parts: &Parts, _: Body, state: Arc<PgPool>) -> Result {
             us.into_response()
         }
         (GET, "/auth") => session(JWT_TOKEN, SESSION_KEY, &parts)?.into_response(),
+
+        (POST, "/login") => {
+            #[derive(Serialize, Deserialize)]
+            struct Login {
+                phone: String,
+                password: String
+            }
+            // LATEST: tracings, return error based on accept header
+            let login = serde_json::from_slice::<Login>(&body.collect().await.unwrap().to_bytes())
+                .bad_request()?;
+
+            login.into_response()
+        }
+
         _ => NOT_FOUND,
     }
 }
